9786061c68cc848efdb70a383f81db332e708c60
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-06-10 23:26:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+describe('example to-do app', () => {
+
+  it('Checking tags', () => {
+    cy.visit('http://localhost:3000'); // update this to your dev server URL
+    cy.get('.testimonial').should('have.length', 2);
+  });
+
+  it('testing parent div', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('.testimonial-section').should('exist');
+  });
+
+  it('testing child divs', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('.testimonial').each(($el) => {
+      cy.wrap($el).find('p').should('have.length', 2); // one text + one author
+    });
+  });
+
+});
+
